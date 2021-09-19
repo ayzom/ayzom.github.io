b97e6851e986f80ee0206342e706b2832beff336
Akhilesh Yadav <aksyvakin@gmail.com>
2021-09-19 16:42:35 +0000

    Add all files

--- dist/_next/static/chunks/webpack.js
+++ dist/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fab63ff18890527144f6";
+/******/ 	var hotCurrentHash = "009c0a90b89326b47eda";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
