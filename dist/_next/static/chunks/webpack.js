3118bb61ab1b03d136519f1606938773892691b6
ayzom <59090251+ayzom@users.noreply.github.com>
2021-09-19 13:30:27 +0000

    Update build files

--- dist/_next/static/chunks/webpack.js
+++ dist/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2946724e4a909ed7192d";
+/******/ 	var hotCurrentHash = "fab63ff18890527144f6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
