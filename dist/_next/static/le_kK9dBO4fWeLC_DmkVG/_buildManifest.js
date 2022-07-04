5e0942db75e67f525577642b3f251895faa8aae1
Akhilesh Yadav <aksyvakin@gmail.com>
2022-07-04 10:30:39 +0000

    push new update

--- dist/_next/static/le_kK9dBO4fWeLC_DmkVG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-0e946077d0ec1ac81bff.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-a82131ad1580845fed48.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
